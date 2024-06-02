4581e8586efc670af1e539ffccb107c86ecc9823
udawale <uddhavdawale2@gmail.com>
2024-06-02 15:29:24 +0530

    Second methd added in controller

--- BookMarkAPI/src/main/java/com/example/demo/rest/BookAPIController.java
+++ BookMarkAPI/src/main/java/com/example/demo/rest/BookAPIController.java
@@ -1,6 +1,7 @@
 package com.example.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,12 @@ public class BookAPIController
 	public String getdata()
 	{
 		return "Welcome ..";
+	}
+	
+	@PostMapping
+	public String postProcess()
+	{
+		return "post data submitted ..";
 	}
 	
 }
